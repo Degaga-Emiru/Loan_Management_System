19ca3ff353e5d73f0e0792a58224626418b8885f
BonnyTW <bon428842@gmail.com>
2025-08-18 12:14:19 +0300

    Initial commit with project files

---
+++ src/main/java/com/BMS/BMS/Controller/BankController.java
@@ -0,0 +1,52 @@
+package com.BMS.BMS.Controller;
+
+import com.BMS.BMS.Service.BankService;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/bank")
+public class BankController {
+
+    private final BankService bankService;
+
+    public BankController(BankService bankService) {
+        this.bankService = bankService;
+    }
+    
+    @GetMapping("/hello")
+    public String Hello() {
+    	return "hello";
+    }
+
+    @PostMapping("/verify")
+    public String verifyAccount(@RequestBody Map<String, String> body) {
+        return bankService.sendMicroDeposit(body.get("accountNumber"));
+    }
+
+    @PostMapping("/verify-deposit")
+    public String verifyMicroDeposit(@RequestBody Map<String, String> payload) {
+        return bankService.confirmMicroDeposit(
+                payload.get("accountNumber"),
+                new BigDecimal(payload.get("microDepositAmount"))
+        );
+    }
+
+    @PostMapping("/loan")
+    public String disburseLoan(@RequestBody Map<String, String> payload) {
+        return bankService.disburseLoan(
+                payload.get("accountNumber"),
+                new BigDecimal(payload.get("loanAmount"))
+        );
+    }
+
+    @PostMapping("/repay")
+    public String repayLoan(@RequestBody Map<String, String> payload) {
+        return bankService.repayLoan(
+                payload.get("accountNumber"),
+                new BigDecimal(payload.get("repaymentAmount"))
+        );
+    }
+}
---
+++ src/main/java/com/BMS/BMS/Reppo/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.BMS.BMS.Reppo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.BMS.BMS.Models.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    
+}
