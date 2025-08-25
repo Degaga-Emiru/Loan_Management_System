c1cb4bba92c16f9218272d468205fb834186d0d9
abenezerdesta <134165120+abenezerdesta@users.noreply.github.com>
2025-08-25 12:26:58 +0300

    Update MyUserDetailService.java

--- LMS/src/main/java/com/LMS/Service/MyUserDetailService.java
+++ LMS/src/main/java/com/LMS/Service/MyUserDetailService.java
@@ -2,12 +2,10 @@ package com.LMS.LMS.Service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import com.LMS.LMS.Controller.UserPrincipal;
 import com.LMS.LMS.Model.Users;
 import com.LMS.LMS.Reppo.UserReppo;
