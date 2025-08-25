689e412f5984ebc26459314f69713c721d7efc45
abenezerdesta <134165120+abenezerdesta@users.noreply.github.com>
2025-08-25 12:28:33 +0300

    Update LoanApplicationRepository.java

--- LMS/src/main/java/com/LMS/Reppo/LoanApplicationRepository.java
+++ LMS/src/main/java/com/LMS/Reppo/LoanApplicationRepository.java
@@ -2,7 +2,6 @@ package com.LMS.LMS.Reppo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.LMS.LMS.Model.LoanApplication;
-
 import java.util.List;
 
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Integer> {
