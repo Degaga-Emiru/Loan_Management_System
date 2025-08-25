228299107ff06f1db38943d576cd325ab2f77611
Segni Merga <137330688+sunerme@users.noreply.github.com>
2025-08-25 12:26:39 +0300

    Update LoanController.java

--- LMS/src/main/java/com/LMS/Controller/LoanController.java
+++ LMS/src/main/java/com/LMS/Controller/LoanController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import jakarta.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.Map;
@@ -134,5 +133,6 @@ public class LoanController {
         return ResponseEntity.ok(loanService.approveLoan(loanApplicationId));
     }
 }
+
 
 
